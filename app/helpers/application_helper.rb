cc5d4efea2ffa31dcbe3f01a455b0221e5e9ddb3
Jesse Farmer <jessethebuilder@yahoo.com>
2015-03-14 01:51:20 -0700

    Generated Devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
+  include FarmTools
+
   #todo include as required methods in farm_tools. Throw method not implemented
   ############################farm _tools####################3
   def do_vendor?
     Rails.env.production?
     true
+  end
 
+  def fb_app_id
+    Dispensary.first.contact_info.social_networking_profile.facebook_app_id if Dispensary.first
   end
 
   ############### end farm_tools########################################
