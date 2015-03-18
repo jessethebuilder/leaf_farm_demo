6cd23215e31d2422fb974f7650dfc73b94b1a44e
Jesse Farmer <jessethebuilder@yahoo.com>
2015-03-17 19:47:45 -0700

    Get Menu to render

--- app/controllers/dispensaries_controller.rb
+++ app/controllers/dispensaries_controller.rb
@@ -12,18 +12,11 @@ class DispensariesController < ApplicationController
     cords = Address.geocode_zip(params[:zip])
     @stores = _leafly_connection.search_locations(cords[0], cords[1])
 
+
+
     #debug
     # @stores = [{'name' => 'Green Orchard', 'slug' => 'green-orchard'}, {'name' => 'Denver Relief', 'slug' => 'denver-relief'}]
   end
 
   private
-
-
-  def custom_menu_order(menu_hash)
-    #put Flower first and Other last
-    h = {}
-    h['Flower'] = menu_hash.delete('Flower')
-    other = menu_hash.delete('Other')
-    h.merge(menu_hash).merge({'Other' => other})
-  end
 end
--- app/controllers/dispensary_menus_controller.rb
+++ app/controllers/dispensary_menus_controller.rb
@@ -1,6 +1,13 @@
 class DispensaryMenusController < ApplicationController
   before_action :set_dispensary, :set_menu, only: [:show]
 
+  rescue_from JSON::ParserError do
+    old_slug = params.delete('leafly_slug')
+    set_dispensary
+    set_menu
+    flash[:alert] = "#{old_slug.titlecase} has not set up a Leafly profile. Please select another dispensary"
+    render :show
+  end
 
   def show
   end
@@ -11,4 +18,6 @@ class DispensaryMenusController < ApplicationController
     #@dispensary is set in ApplicationController
     @menu = @dispensary.dispensary_menu
   end
+
+
 end
